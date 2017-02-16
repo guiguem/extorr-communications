df3965868db994d55cc55bf7e0c55cabb03fed11
Guigue, Mathieu <mathieu.guigue@pnnl.gov>
2017-02-15 19:20:40 -0800

    Reading arguments and extract command to send

--- src/provider.cs
+++ src/provider.cs
@@ -1,9 +1,33 @@
 using System;
 
-class app
+class Provider
 {
-  static public void Main ()
+  string SelectArgument(string[] args)
+  {
+    if (args.Length < 1 )
+    {
+      Console.WriteLine ("fatal: Not enough arguments");
+      System.Environment.Exit(1);
+    }
+    else
+    {
+      Console.WriteLine("info: Will send {0}",args[0]);
+    }
+    if (args.Length > 1 )
+    {
+      string text = "info: Will ignore following arguments:";
+      for (int i=1; i<args.Length; i = i + 1)
       {
-          Console.WriteLine ("Hello Mono World");
+        text = text + " " + args[i];
+      }
+      Console.WriteLine(text);
+    }
+    return args[0];
+  }
+  static public void Main (string[] args)
+      {
+          Console.WriteLine ("info: Hello Mono World");
+          Provider provider = new Provider();
+          string command = provider.SelectArgument(args);
       }
 }
